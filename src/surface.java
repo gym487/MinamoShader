3eb41ebcb1ba1b63fedbb0a9a03d8ed0c912fdb1
gym487 <gym487@163.com>
2017-02-10 23:30:33 +0800

    vector

--- src/surface.java
+++ src/surface.java
@@ -1,21 +1,28 @@
 
 public abstract class surface {
-	private double r,g,b;
-	 private static int glass=1;
-	 private static int mirror=2;
-	 private static int diffuse=3;
-	 private static int tri =1;
-	 private static int squ=2;
-	 private int m;
-	 private int t;
-	private vec p,u,v,n;//point u v normal;
+	 double r,g,b;
+	 vec p,u,v,n;//point u v normal;
 	public surface(vec p,vec u,vec v,int m,int t,double r,double g,double b){
 		this.p=p;
 		this.u=u;
 		this.v=v;
 		this.n=vec.cro(u, v);
-		this.m=m;
-		this.t=t;
+		this.r=r;
+		this.g=g;
+		this.b=b;
+	}
+}
+
+class diffSurface extends surface{
+}
+
+class mirrSurface extends surface{
+	
+	public mirrSurface(vec p,vec u,vec v,int m,int t,double r,double g,double b){
+		this.p=p;
+		this.u=u;
+		this.v=v;
+		this.n=vec.cro(u, v);
 		this.r=r;
 		this.g=g;
 		this.b=b;
