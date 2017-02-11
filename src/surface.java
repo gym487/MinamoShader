fa9ac575832ef67e2df673543fdfd9b890262110
gym487 <gym487@163.com>
2017-02-11 16:14:32 +0800

    surface construction

--- src/surface.java
+++ src/surface.java
@@ -1,24 +1,12 @@
 
 public abstract class surface {
-	 double r,g,b;
+	 
 	 vec p,u,v,n;//point u v normal;
-	public surface(vec p,vec u,vec v,int m,int t,double r,double g,double b){
-		this.p=p;
-		this.u=u;
-		this.v=v;
-		this.n=vec.cro(u, v);
-		this.r=r;
-		this.g=g;
-		this.b=b;
-	}
 }
 
 class diffSurface extends surface{
-}
-
-class mirrSurface extends surface{
-	
-	public mirrSurface(vec p,vec u,vec v,int m,int t,double r,double g,double b){
+	double r,g,b;
+	public diffSurface(vec p,vec u,vec v,int m,int t,double r,double g,double b){
 		this.p=p;
 		this.u=u;
 		this.v=v;
@@ -27,4 +15,23 @@ class mirrSurface extends surface{
 		this.g=g;
 		this.b=b;
 	}
+}
+
+class mirrSurface extends surface{
+	double rf,gf,bf;
+	double rt,gt,bt;
+	public mirrSurface(vec p,vec u,vec v,int m,int t,double rf,double gf,double bf,double rt,double gt,double bt){
+		this.p=p;
+		this.u=u;
+		this.v=v;
+		this.n=vec.cro(u, v);
+		this.rf=rf;
+		this.gf=gf;
+		this.bf=bf;
+		this.rt=rt;
+		this.gt=gt;
+		this.bt=bt;
+	}
+	
+	
 }
