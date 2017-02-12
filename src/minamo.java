bcfa372740685947afdf89d86d57834fa15d7ea1
gym487 <gym487@163.com>
2017-02-12 18:45:25 +0800

    some tips

--- src/minamo.java
+++ src/minamo.java
@@ -14,13 +14,14 @@ public  class minamo extends Thread {
 		 if(flag==1) return false;
 		 else return true;
 	}
-	public  static void main(String arg[]){
+	public  static void main(String arg[]){//Test scene below
+		//TODO: Read scene from text file.
 		cam c=new cam(new vec(0,0,0),new vec(0,0,1),new vec(0,1,0),500,500);
 		scene=new surface[3];
 		threadn=4;
 		thok=new int[threadn];
 		threads=new thread[threadn];
-		scene[2]=mirrSurface.newmirr(new vec(-4,-4,12),new vec(-4,4,12),new vec(0,-4,17),new spec(0.6,0.6,0.6),1);
+		scene[2]=mirrSurface.newmirr(new vec(-3,-4,12),new vec(-3,4,12),new vec(0,-4,17),new spec(0.6,0.6,0.6),1);
 		//scene[2]=mirrSurface.newmirr(new vec(-4,-4,12.001),new vec(0,-4,17.001),new vec(-4,4,12.001),new spec(0.6,0.6,0.6),1);
 		scene[0]=diffSurface.newdiff(new vec(40,4,160),new vec(-40,4,160),new vec(40,4,1),new spec(0.1,0.5,0.1),1);
 		scene[1]=lightSurface.newlight(new vec(4,-4,16),new vec(4,4,16),new vec(4,-4,10),new spec(1000,1000,1000),1);
